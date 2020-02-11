ba6bb8f7f815220fadb2441f06d6b9fdc932b00c
Nadia-Kadri <nadiagrace96@gmail.com>
2020-02-11 12:47:47 -0500

    create react app and Header component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <div>App</div>
+      </div>
+    );
+  }
+}
+
+export default App;
